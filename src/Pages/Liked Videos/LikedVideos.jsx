436f0a856e7747c87558b339a7c768dbf5c6aedd
ankushpndt <ankushpndt@gmail.com>
2022-04-15 08:50:40 +0530

    style: removed margin

--- src/Pages/Liked Videos/LikedVideos.jsx
+++ src/Pages/Liked Videos/LikedVideos.jsx
@@ -1,85 +1,88 @@
-import React, { useEffect } from 'react';
-import { Loader } from '../../Components/Loader';
-import { Link } from 'react-router-dom';
-import '../../Videos.css';
-import { useData } from '../../Context/DataContext';
+import React, { useEffect } from "react";
+import { Loader } from "../../Components/Loader";
+import { Link } from "react-router-dom";
+import "../../Videos.css";
+import { useData } from "../../Context/DataContext";
 import {
-  getLikedVideos,
-  deleteFromLikedVideos,
-  addToLikedByUser,
-} from '../../utils/ApiCall';
-import { useAuth } from '../../Context/AuthContext';
+	getLikedVideos,
+	deleteFromLikedVideos,
+	addToLikedByUser,
+} from "../../utils/ApiCall";
+import { useAuth } from "../../Context/AuthContext";
 
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 export const LikedVideos = () => {
-  const { likedVideo, dispatch, videoList ,setLoader,loader} = useData();
-  const { token, userId } = useAuth();
-  const extractVideoFromLikedVideos = videoList?.filter((video) => {
-    return likedVideo?.find((el) => el._id === video._id);
-  });
+	const { likedVideo, dispatch, videoList, setLoader, loader } = useData();
+	const { token, userId } = useAuth();
+	const extractVideoFromLikedVideos = videoList?.filter((video) => {
+		return likedVideo?.find((el) => el._id === video._id);
+	});
 
-  useEffect(() => {
-    if (token) {
-      getLikedVideos(dispatch, token,setLoader);
-    }
-  }, [dispatch, token,setLoader]);
-  return (
-    <main>
-      <h1>Liked Videos</h1>
-      {!loader?<div className='video__item'>
-        <ul className='history__list'>
-          {extractVideoFromLikedVideos.length !== 0 ? (
-            extractVideoFromLikedVideos.map((video, i) => {
-              return (
-                <div className='container' key={i}>
-                  <button
-                    className='liked__remove__btn'
-                    onClick={() => {
-                      deleteFromLikedVideos({
-                        dispatch,
-                        token,
-                        _id: video?._id,
-                      });
-                      addToLikedByUser({
-                        dispatch,
-                        _id: video?._id,
-                        token,
-                        userId,
-                      });
-                    }}
-                  >
-                    <DeleteIcon />
-                  </button>
+	useEffect(() => {
+		if (token) {
+			getLikedVideos(dispatch, token, setLoader);
+		}
+	}, [dispatch, token, setLoader]);
+	return (
+		<main>
+			<h1>Liked Videos</h1>
+			{!loader ? (
+				<div className="video__item">
+					<ul className="history__list">
+						{extractVideoFromLikedVideos.length !== 0 ? (
+							extractVideoFromLikedVideos.map((video, i) => {
+								return (
+									<div className="container" key={i}>
+										<button
+											className="liked__remove__btn"
+											onClick={() => {
+												deleteFromLikedVideos({
+													dispatch,
+													token,
+													_id: video?._id,
+												});
+												addToLikedByUser({
+													dispatch,
+													_id: video?._id,
+													token,
+													userId,
+												});
+											}}
+										>
+											<DeleteIcon />
+										</button>
 
-                  <Link
-                    style={{
-                      textDecoration: 'none',
-                      color: 'black',
-                      display: 'grid',
-                      marginBottom: '1rem',
-                    }}
-                    to={`/video/${video?.videoId}`}
-                    key={video.id}
-                  >
-                    {' '}
-                    <div className='liked__video__body'>
-                      <img src={video?.image} alt='error' />
-                      <div style={{ width: '100%', paddingTop: '1rem' }}>
-                        {video?.title}
-                      </div>
-                      <p>
-                        {video?.views} • <span>{video?.date}</span>
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              );
-            })
-          ) : (
-            <div className='empty__text'>There is no video here.</div>
-          )}
-        </ul>
-      </div>:<Loader/>}
-    </main>
-  );
+										<Link
+											style={{
+												textDecoration: "none",
+												color: "black",
+												display: "grid",
+											}}
+											to={`/video/${video?.videoId}`}
+											key={video.id}
+										>
+											{" "}
+											<div className="liked__video__body">
+												<img src={video?.image} alt="error" />
+												<div style={{ width: "100%", paddingTop: "1rem" }}>
+													{video?.title}
+												</div>
+												<p>
+													{video?.views} • <span>{video?.date}</span>
+												</p>
+											</div>
+										</Link>
+									</div>
+								);
+							})
+						) : (
+							<div className="empty__text">There is no video here.</div>
+						)}
+					</ul>
+				</div>
+			) : (
+				<Loader />
+			)}
+		</main>
+	);
 };
--- src/Pages/Watch Later/WatchLater.jsx
+++ src/Pages/Watch Later/WatchLater.jsx
@@ -1,67 +1,70 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useData } from '../../Context/DataContext';
-import { getWatchLater, deleteFromWatchLater } from '../../utils/ApiCall';
-import { useAuth } from '../../Context/AuthContext';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Loader } from '../../Components/Loader';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useData } from "../../Context/DataContext";
+import { getWatchLater, deleteFromWatchLater } from "../../utils/ApiCall";
+import { useAuth } from "../../Context/AuthContext";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Loader } from "../../Components/Loader";
 export const WatchLater = () => {
-  const { watchLater, dispatch, videoList,setLoader,loader } = useData();
-  const { token } = useAuth();
-  const extractVideoFromWatchLater = videoList?.filter((video) => {
-    return watchLater?.find((el) => el._id === video._id);
-  });
-  useEffect(() => {
-    getWatchLater(dispatch, token,setLoader);
-  }, [dispatch, token,setLoader]);
-  return (
-    <main>
-      <h1>Watch Later</h1>
-      {!loader?<div className='video__item'>
-        <ul className='history__list'>
-          {extractVideoFromWatchLater?.length > 0 ? (
-            extractVideoFromWatchLater?.map((video, i) => {
-              return (
-                <div className='container' key={i}>
-                  <button
-                    className='liked__remove__btn'
-                    onClick={() =>
-                      deleteFromWatchLater({
-                        dispatch,
-                        token,
-                        _id: video?._id,
-                      })
-                    }
-                  >
-                    <DeleteIcon />
-                  </button>
-                  <Link
-                    style={{
-                      textDecoration: 'none',
-                      color: 'black',
-                      display: 'grid',
-                      marginBottom: '1rem',
-                    }}
-                    to={`/video/${video?.videoId}`}
-                    key={video?.id}
-                  >
-                    {' '}
-                    <div className='liked__video__body'>
-                      <img src={video?.image} alt='error' />
-                      <div style={{ marginTop: '1rem' }}>{video?.title}</div>
-                      <p>
-                        {video?.views} • <span>{video?.date}</span>
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              );
-            })
-          ) : (
-            <div className='empty__text'>There is no video here.</div>
-          )}
-        </ul>
-      </div>:<Loader/>}
-    </main>
-  );
+	const { watchLater, dispatch, videoList, setLoader, loader } = useData();
+	const { token } = useAuth();
+	const extractVideoFromWatchLater = videoList?.filter((video) => {
+		return watchLater?.find((el) => el._id === video._id);
+	});
+	useEffect(() => {
+		getWatchLater(dispatch, token, setLoader);
+	}, [dispatch, token, setLoader]);
+	return (
+		<main>
+			<h1>Watch Later</h1>
+			{!loader ? (
+				<div className="video__item">
+					<ul className="history__list">
+						{extractVideoFromWatchLater?.length > 0 ? (
+							extractVideoFromWatchLater?.map((video, i) => {
+								return (
+									<div className="container" key={i}>
+										<button
+											className="liked__remove__btn"
+											onClick={() =>
+												deleteFromWatchLater({
+													dispatch,
+													token,
+													_id: video?._id,
+												})
+											}
+										>
+											<DeleteIcon />
+										</button>
+										<Link
+											style={{
+												textDecoration: "none",
+												color: "black",
+												display: "grid",
+											}}
+											to={`/video/${video?.videoId}`}
+											key={video?.id}
+										>
+											{" "}
+											<div className="liked__video__body">
+												<img src={video?.image} alt="error" />
+												<div style={{ marginTop: "1rem" }}>{video?.title}</div>
+												<p>
+													{video?.views} • <span>{video?.date}</span>
+												</p>
+											</div>
+										</Link>
+									</div>
+								);
+							})
+						) : (
+							<div className="empty__text">There is no video here.</div>
+						)}
+					</ul>
+				</div>
+			) : (
+				<Loader />
+			)}
+		</main>
+	);
 };
